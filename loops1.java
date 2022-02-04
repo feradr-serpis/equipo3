ad52efa69641f5058cbf37063d47b9e10cb9bb5d
Gemper <95484337+Gemper@users.noreply.github.com>
2022-02-04 19:10:54 +0100

    Update loops1.java
    He modificado el archivo

--- loops1.java
+++ loops1.java
@@ -1,14 +1,23 @@
 
-public class loops1 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-        int i;
-        for(i=1;i<=10;i)
-        {
-        }
-        
-        bufferedReader.close();
-    }
+public class tabla 
+{
+	 public static void main(String[] args) throws IOException 
+	 {
+	    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+	        int N = Integer.parseInt(bufferedReader.readLine().trim());
+	        int i;
+	        for(i=1;i<=10;i++)
+	        {
+	        	System.out.println(N+"x"+i+"="+(N*i));
+	        }
+	        
+	        bufferedReader.close();
+	    }
+	
+
 }
